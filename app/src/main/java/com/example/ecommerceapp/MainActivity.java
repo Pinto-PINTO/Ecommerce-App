c7247465baef92b155a5142b2c667c9c16b435a1
Menuka Pinto <instafeed9999@gmail.com>
2021-08-22 00:31:00 +0530

    Implemented buttons to move between login and register

--- app/src/main/java/com/example/ecommerceapp/MainActivity.java
+++ app/src/main/java/com/example/ecommerceapp/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.ecommerceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Components
+    TextView login_email;
+    TextView login_password;
+    TextView register;
+    Button login_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login_email = findViewById(R.id.login_email);
+        login_password = findViewById(R.id.login_password);
+        register = findViewById(R.id.register);
+        login_btn = findViewById(R.id.login_btn);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent register_intent = new Intent(MainActivity.this, RegisterUser.class);
+                startActivity(register_intent);
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/ecommerceapp/RegisterUser.java
+++ app/src/main/java/com/example/ecommerceapp/RegisterUser.java
@@ -2,13 +2,30 @@ package com.example.ecommerceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class RegisterUser extends AppCompatActivity {
+
+    // Components
+    ImageButton back_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
+
+        back_btn = (ImageButton) findViewById(R.id.back_btn);
+
+        back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent back_btn_Intent = new Intent(RegisterUser.this, MainActivity.class);
+                startActivity(back_btn_Intent);
+            }
+        });
+
     }
 }
